0d57c3de06f908711c9b20b8d48cf6c994118720
Marek Fišera <maraf@users.noreply.github.com>
2011-10-14 14:11:14 +0000

    Začátek doménového modelu

---
+++ carshare-domain/src/com/carshare/domain/User.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.carshare.domain;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Mara
+ */
+public class User {
+    private int id;
+    private String username;
+    private String password;
+
+    private String email;
+    private String firstName;
+    private String lastName;
+
+    private String phoneNumber;
+    private String address;
+    private String carDescription;
+    private Date registrated;
+    private boolean certified;
+}

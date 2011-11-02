6a68fb33b4e9239752564ec2b8221501109de094
aduss4@gmail.com <aduss4@gmail.com@c12a7911-592f-2a58-910c-a58d3ce51ca4>
2011-11-02 16:29:26 +0000

    založení projektu pro web

---
+++ carshare-webclient/src/com/carshare/webclient/Carshare.java
@@ -0,0 +1,15 @@
+package com.carshare.webclient;
+
+import com.google.gwt.core.client.EntryPoint;
+
+
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class Carshare implements EntryPoint {
+	
+	public void onModuleLoad() {
+		
+	}
+}

0daaa347263ddcc7424e8c1f76caa883aa9de3f4
Marek Fišera <maraf@users.noreply.github.com>
2012-05-06 10:21:26 +0000

    Test console upgrade

--- carshare-domain/src/com/carshare/domain/dto/TripUserRequest.java
+++ carshare-domain/src/com/carshare/domain/dto/TripUserRequest.java
@@ -1,16 +1,7 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.carshare.domain.dto;
 
 import com.neptuo.service.io.annotation.*;
 
-/**
- *
- * @author Mara
- */
 @Serializable(name="trip-user-request")
 @Deserializable(name="trip-user-request")
 public class TripUserRequest implements java.io.Serializable {
